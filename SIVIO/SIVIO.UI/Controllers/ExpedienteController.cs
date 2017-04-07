7286399b0f1b22c729f5a47f82fc57257924396f
Jonathan Gomez <jgomez@inamu.go.cr>
2017-04-07 07:32:51 -0600

    Agregado vistas para formularios de seguridad

---
+++ SIVIO/SIVIO.UI/Controllers/ExpedienteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIVIO.UI.Controllers
+{
+    public class ExpedienteController : Controller
+    {
+        // GET: Expediente
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SIVIO/SIVIO.UI/Controllers/SeguridadController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SIVIO.UI.Controllers
+{
+    public class SeguridadController : Controller
+    {
+        // GET: Seguridad
+        public ActionResult GridUsuarios()
+        {
+            return View();
+        }
+
+        public ActionResult GridRoles()
+        {
+            return View();
+        }
+        public ActionResult GridBitácora()
+        {
+            return View();
+        }
+        public ActionResult GridTarjetasInteligentes()
+        {
+            return View();
+        }
+    }
+}
