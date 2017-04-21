f6d377bffac41e4520ef4b10803eab2aec16ec91
Konrad Jimenez Chavarria <b33504@ecci.ucr.ac.cr>
2017-04-21 10:23:32 -0600

    Agregados los tags del menu para delegacion de la mujer

--- SIVIO/SIVIO.UI/Controllers/HomeController.cs
+++ SIVIO/SIVIO.UI/Controllers/HomeController.cs
@@ -93,7 +93,7 @@ namespace SIVIO.UI.Controllers
             //usuario.IM_SALT2 = salt2;
             //_modelSeguridad.InsertarUsuario(usuario);
             return View();
-        }
+            }
 
         [AllowAnonymous]
         public JsonResult AutenticarUsuario(string usuario, string clave, int tipoServicio)
