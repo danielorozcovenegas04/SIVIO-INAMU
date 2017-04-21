a86ef08fb6917946736d33be214a545acfd9edf6
Josue Valverde Sanchez <b16802@ecci.ucr.ac.cr>
2017-04-21 11:38:12 -0600

    Restaurar carpeta entrega

---
+++ SIVIO Entrega INAMU/COMUN_MODELO/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos atributos para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Comun_modelo")]
+[assembly: AssemblyDescription("modelo")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GRUPO BABEL")]
+[assembly: AssemblyProduct("INAMU")]
+[assembly: AssemblyCopyright("INAMU")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible como false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM. Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible como true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+[assembly: Guid("23971ac4-52d7-4910-8c1b-47b5c502c4ee")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
