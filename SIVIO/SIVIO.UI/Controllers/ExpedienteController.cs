3e7f9f83166012404fdb29f88022f6b03d012a37
Bryan Sibaja Granados <bryan.sibaja@ucr.ac.cr>
2017-04-25 11:47:13 -0600

    Catalogos funcionales y actualización de pestañas segun el prototipo

--- SIVIO/SIVIO.UI/Controllers/ExpedienteController.cs
+++ SIVIO/SIVIO.UI/Controllers/ExpedienteController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SIVIO.Entidades;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -94,6 +96,28 @@ namespace SIVIO.UI.Controllers
         {
             var catalogoServicios = _modelCatalogos.ObtenerCatalogoPorId((int)Utilitarios.Enumerados.EnumCatalogos.Nacionalidad);
             return View();
+        }
+
+        [AllowAnonymous]
+        public JsonResult CargarCatalogos()
+        {
+            dynamic objetoRetorno = new ExpandoObject();
+            try
+            {
+                objetoRetorno.Mensaje = new Mensaje((int)Mensaje.CatTipoMensaje.Exitoso, string.Empty, string.Empty);
+                objetoRetorno.Catalogo = _modelCatalogos.ObtenerCatalogoPorId((int)Utilitarios.Enumerados.EnumCatalogos.Nacionalidad).
+                    TBL_VALOR_CATALOGO.Select(m=> new { m.PK_VALORCATALOGO,m.VC_VALOR1,m.VC_VALOR2});
+                objetoRetorno.CatalogoCondicionMigratoria = _modelCatalogos.ObtenerCatalogoPorId((int)Utilitarios.Enumerados.EnumCatalogos.SituacionMigratoria).
+                    TBL_VALOR_CATALOGO.Select(m=> new { m.PK_VALORCATALOGO,m.VC_VALOR1,m.VC_VALOR2});
+                objetoRetorno.CatalogoDiscapasidades = _modelCatalogos.ObtenerCatalogoPorId((int)Utilitarios.Enumerados.EnumCatalogos.TipoDiscapacidad).
+                    TBL_VALOR_CATALOGO.Select(m=> new { m.PK_VALORCATALOGO,m.VC_VALOR1,m.VC_VALOR2});
+                return Json(Newtonsoft.Json.JsonConvert.SerializeObject(objetoRetorno), JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                objetoRetorno.Mensaje = new Mensaje((int)Mensaje.CatTipoMensaje.Error,"Error al cargar catalogos", string.Empty);
+                return Json(Newtonsoft.Json.JsonConvert.SerializeObject(objetoRetorno), JsonRequestBehavior.AllowGet);
+            }
         }
         #endregion
     }
